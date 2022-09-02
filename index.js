940eaa3d3bb604bad23b8f4d15397b7d7880c49e
foysal1021 <foysala1021@gmail.com>
2022-09-03 00:04:03 +0600

    fetch added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// const brackingBtn
-// const regularBtn
-// const InternationalBtn
-// const sportsBtn
-// const EntertainmentBtn
-// const Culture
-// const Arts
-// const onclick
+// all Category is start here
+const allCategory = ()=>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(catagory => CategoryInfo(catagory.data.news_category))
+}
+// all Category is end here
+

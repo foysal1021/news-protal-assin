ed78bd33d29b4eae89f3705bc665a6b509783065
foysal1021 <foysala1021@gmail.com>
2022-09-03 00:05:25 +0600

    manu btn added

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const allCategory = ()=>{
 }
 // all Category is end here
 
+
+//Category info is start here
+const CategoryInfo = (rcvInfo)=>{
+    const ManuBtn = document.getElementById('all-manu-btn');
+    //loop is start here
+    rcvInfo.forEach(info =>{
+    const ManuBtnDiv = document.createElement('div');
+    ManuBtnDiv.innerHTML = ` <button onclick="allCategoryBTN('${info.category_id}')" class="btn btn-success"> ${info.category_name} </button>`;
+    ManuBtn.appendChild(ManuBtnDiv);
+    });
+    //loop is end here
+}
+//Category info is end here
+allCategory();
+
+

3bacd22348ba97a10644395cf8fd43f3297986dd
foysal1021 <foysala1021@gmail.com>
2022-09-04 03:33:47 +0600

    yes done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // all Category is start here
-const allCategory = ()=>{
-  try{
+const allCategory = () => {
+  try {
     fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(catagory => CategoryInfo(catagory.data.news_category))
-  }catch(e){
+      .then(res => res.json())
+      .then(catagory => CategoryInfo(catagory.data.news_category))
+  } catch (e) {
     console.log(e);
   }
 }
@@ -12,126 +12,120 @@ const allCategory = ()=>{
 
 
 //Category info is start here
-const CategoryInfo = (rcvInfo)=>{
-
-    const ManuBtn = document.getElementById('all-manu-btn');
-    //loop is start here
-    rcvInfo.forEach(info =>{
+const CategoryInfo = (rcvInfo) => {
+  const ManuBtn = document.getElementById('all-manu-btn');
+  //loop is start here
+  rcvInfo.forEach(info => {
     const ManuBtnDiv = document.createElement('div');
     ManuBtnDiv.classList.add('text-center');
     ManuBtnDiv.innerHTML = ` <button onclick="allCategoryBTN('${info.category_id}')" class="btn btn-success w-100 mt-2"> ${info.category_name} </button>`;
     ManuBtn.appendChild(ManuBtnDiv);
-    });
-    //loop is end here
-
-
+  });
+  //loop is end here
 }
-//Category info is end here
 allCategory();
-
+//Category info is end here
 
 
 // all Category BTN start here
-const allCategoryBTN = (rcv_category_id)=>{
-    document.getElementById('spinner').classList.remove('d-none')
-    const url = ` https://openapi.programming-hero.com/api/news/category/${rcv_category_id}`;
-    try{
-      fetch (url)
+const allCategoryBTN = (rcv_category_id) => {
+  document.getElementById('spinner').classList.remove('d-none')
+  const url = ` https://openapi.programming-hero.com/api/news/category/${rcv_category_id}`;
+  try {
+    fetch(url)
       .then(res => res.json())
       .then(category => displayNews(category.data));
-    }
-    catch(e){
-      console.log(e);
-    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 // all Category BTN end here
 
+
 //display news start here
-const displayNews = (category)=>{
-  if(category.length<1){
-    document.getElementById('spinner').classList.add('d-none');
+const displayNews = (category) => {
+  category.sort((a, b) => b.total_view - a.total_view); // top views serial
+
+  if (category.length < 1) {
+    document.getElementById('spinner').classList.add('d-none'); //stop spinner for  Culture btn
   }
-    document.getElementById('news-length').innerText = category.length; // this search total regult length
-    const NewsContainer = document.getElementById('news-container');
-    NewsContainer.innerHTML = "";
-    category.forEach( news =>{
-      console.log(news)
+  document.getElementById('news-length').innerText = category.length; // new search length show 
+  const NewsContainer = document.getElementById('news-container');
+  NewsContainer.innerHTML = "";
+  category.forEach(news => {
+    console.log(news)
     const NewsDiv = document.createElement("div");
     NewsDiv.classList.add('card');
     NewsDiv.classList.add('mb-3');
     NewsDiv.innerHTML =
-    `
-    <div onclick="GetNewsId('${news._id}')" class="row g-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    <div class="col-md-3 text-lg-start text-center">
-      <img src="${news.thumbnail_url}" class="img-fluid rounded-start h-100 " alt="...">
+      `<div onclick="GetNewsId('${news._id}')" class="row g-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+     <div class="col-md-3 text-lg-start text-center">
+    <img src="${news.thumbnail_url}" class="img-fluid rounded-start h-100 " alt="...">
     </div>
 
     <div class="col-md-9">
       <div class="card-body">
         <h4 class="card-title text-center text-lg-start"> ${news.title} </h4>
-        <p class="card-text text-center my-3 text-lg-start my-lg-0"> ${news.details.slice(0,400)}...... </p>
-
+           <p class="card-text text-center my-3 text-lg-start my-lg-0"> ${news.details.slice(0,400)}...... </p>
         <div class=" d-block d-lg-flex align-items-center mt-5">
-
-             <div class="d-flex col-12 col-lg-4 justify-content-center justify-content-lg-start">
-              <img class="repoter-img" src="${news.author.img}"/>
-              <div>
-              <h5 class="m-0 ms-3">${news.author.name?news.author.name:'no name found'}</h5>
-              <p class="ms-3"> ${news.author.published_date?news.author.published_date:'no date found'} </p>
+          <div class="d-flex col-12 col-lg-5 justify-content-center justify-content-lg-start">
+            <img class="repoter-img" src="${news.author.img}"/>
+            <div>
+          <h5 class="m-0 ms-3">${news.author.name?news.author.name:'no name found'}</h5>
+            <p class="ms-3"> ${news.author.published_date?news.author.published_date:'no date found'} </p>
               </div>
-
-              </div>
-
-             <div class="">
-             <p class="mt-3 text-center mt-lg-0"><i class="fa-sharp fa-solid fa-eye"></i> ${news.total_view?news.total_view:'no views'} </P>
+            </div>
+          <div class="">
+            <p class="mt-3 text-center mt-lg-0"><i class="fa-sharp fa-solid fa-eye"></i> ${news.total_view?news.total_view:'no views'} </P>
              </div>
-
         </div>
-
       </div>
     </div>
 
   </div>
     `;
-
     document.getElementById('spinner').classList.add('d-none')
     NewsContainer.appendChild(NewsDiv);
-    });
-    
+  });
 }
+
 // modal 
-const GetNewsId = (rcvNewsId)=>{
+const GetNewsId = (rcvNewsId) => {
   const url = `https://openapi.programming-hero.com/api/news/${rcvNewsId}`;
-  try{
+  try {
     fetch(url)
-    .then(res => res.json())
-    .then(newsid =>getModalData(newsid.data))
-  }
-  catch(e){
+      .then(res => res.json())
+      .then(newsid => getModalData(newsid.data))
+  } catch (e) {
     console.log(e);
   }
 }
-
-const getModalData = (rcvNewsID)=>{
+const getModalData = (rcvNewsID) => {
   console.log(rcvNewsID[0]);
   const modalBody = document.getElementById('modalBody');
-  modalBody.innerHTML=
-  `
+  modalBody.innerHTML =
+    `
   <img class="w-100" src='${rcvNewsID[0].image_url}'/>
+  <h6> this is today photo : ${rcvNewsID[0].others_info.is_todays_pick}</h6>
+  <p> category id: ${rcvNewsID[0].category_id} </P>
   <p> _id: ${rcvNewsID[0]._id} </P>
   <h5> ${rcvNewsID[0].title} </h5>
   <p> ${rcvNewsID[0].details} </p>
-  <h5> Repoter : ${rcvNewsID[0].author.name?rcvNewsID[0].author.name: 'no name found'} </h5>
+  <div class="d-flex align-items-center my-2"> 
+  <img style="width:50px;border-radius:25px" src="${rcvNewsID[0].author.img}"/>
+  <h5 class="ms-2"> Repoter : ${rcvNewsID[0].author.name?rcvNewsID[0].author.name: 'no name found'} </h5>
+  </div>
   <p> published date : ${rcvNewsID[0].author.published_date?rcvNewsID[0].author.published_date:'no date found'} </p>
   <h5> Total Views : ${rcvNewsID[0].total_view?rcvNewsID[0].total_view:"no views"} </h5>
+  <p> this is tranding news? : ${rcvNewsID[0].others_info.is_trending}</p>
   `
 };
 // modal is end here
 
 
 
-// linded with blog.html form index.html
-const newBlog = ()=>{
+// linked with blog.html form index.html
+const newBlog = () => {
   document.getElementById('spinner').classList.remove('d-none');
   window.open('http://127.0.0.1:5500/blog.html');
   document.getElementById('spinner').classList.add('d-none');
